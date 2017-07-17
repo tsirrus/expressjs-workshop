c81b17ce6eada44515a32d14d289c729e9b47fc9
TSirrus <tsirrus@gmail.com>
2017-07-17 20:11:26 +0000

    Done ex8

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var app = express();
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set('view engine', 'pug');
+app.use('/files', express.static('static_files'));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');

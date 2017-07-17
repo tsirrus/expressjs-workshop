2b8a02262b2db4d5e4e54ccfea5a2fce49110489
TSirrus <tsirrus@gmail.com>
2017-07-17 17:45:49 +0000

    Done ex3

--- index.js
+++ index.js
@@ -12,6 +12,39 @@ app.get('/hello', function(request,response) {
   response.end('<h1>Hello World!</h1>');
 });
 
+app.get('/calculator/:operation', function(request, response) {
+  var num1 = 0;
+  var num2 = 0;
+  if (request.query.num1 !== undefined) {
+    num1 = Number(request.query.num1);
+  }
+  
+  if (request.query.num2 !== undefined) {
+    num2 = Number(request.query.num2);
+  }
+  
+  var solution;
+  switch(request.params.operation) {
+    case 'add':
+      solution = num1 + num2;
+      break;
+    case 'multiply':
+      solution = num1 * num2;
+      break;
+    default:
+      response.end("400 Bad Request");
+  };
+    
+    var solutionObject = {
+      operation: request.params.operation,
+      firstOperand: request.query.num1,
+      secondOperand: request.query.num2,
+      solution: solution
+    };
+    
+    response.end(JSON.stringify(solutionObject, null, 2));
+});
+
 
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
